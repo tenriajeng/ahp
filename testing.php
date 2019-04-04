d3dc1b2c29cc996992bbb87d1ac5e67ae5fed83e
tenriajeng <ilhamtenriajeng03@gmail.com>
2019-04-04 20:19:32 +0800

    ahp new update

--- testing.php
+++ testing.php
@@ -34,37 +34,38 @@ $data = array(
         1 / 3,
         1 / 3,
         1 / 3,
-        1 / 3,
-    )
-   
-);
+        1,
+    ),
 
-function ahpku($data1)
-{
-    $jumlah = count($data1[0]);
-    
-    $h2  = 0;
+);
+    $dataH;
+
+    $jumlah = count($data[0]);
+
+    $h2 = 0;
     $ptl = array();
+    $ba=0;
     for ($c = 0; $c < $jumlah; $c++) {
         $h = 0;
         for ($a = 0; $a < $jumlah; $a++) {
             $g = 0;
             for ($b = 0; $b < $jumlah; $b++) {
-                $e = $data1[$c][$b];
-                $d = $data1[$b][$a];
+                $e = $data[$c][$b];
+                $d = $data[$b][$a];
                 $f = $e * $d;
                 $g = $g + $f;
-                
             }
 
-            echo "<u>" . ($g) . "</u>&nbsp;&nbsp;&nbsp;";
-            $h  = $h + $g;
-            $h2 = $h2 + $g;
+            $dataH[$ba] = $g;
+            $ba++;
 
-            
+
+            echo "<u>" . ($g) . "</u>&nbsp;&nbsp;&nbsp;";
+            $h = $h + $g;
+            $h2 = $h2 + $g;
         }
         $ptl[$c] = $h;
-        
+
         echo $h;
         echo "<br>";
     }
@@ -72,11 +73,54 @@ function ahpku($data1)
     $hasil = 0;
     foreach ($ptl as $ito) {
         $hasil = $hasil + $ito / $h2;
-        
-    }
-    
-    echo $hasil;
-}
 
-ahpku($data);
-?>
+    }
+
+    
+
+    $dataH2;
+
+    echo $hasil."<br>";
+
+    $tam=0;
+    for ($i=0; $i < 5 ; $i++) { 
+        for ($u=0; $u < 5 ; $u++) { 
+            $dataH2[$i][$u] = $dataH[$tam];
+            $tam++;
+            // echo "&nbsp;<br>";
+        }
+    }
+
+  
+    $jumlah = count($dataH2[0]);
+
+    $h2 = 0;
+    $ptl = array();
+    $ba=0;
+    for ($c = 0; $c < $jumlah; $c++) {
+        $h = 0;
+        for ($a = 0; $a < $jumlah; $a++) {
+            $g = 0;
+            for ($b = 0; $b < $jumlah; $b++) {
+                $e = $dataH2[$c][$b];
+                $d = $dataH2[$b][$a];
+                $f = $e * $d;
+                $g = $g + $f;
+            }
+
+            echo "<u>" . ($g) . "</u>&nbsp;&nbsp;&nbsp;";
+            $h = $h + $g;
+            $h2 = $h2 + $g;
+        }
+        $ptl[$c] = $h;
+
+        echo $h;
+        echo "<br>";
+    }
+    echo $h2 . "<br><br>";
+    $hasil = 0;
+    foreach ($ptl as $ito) {
+        $hasil = $hasil + $ito / $h2;
+
+    }
+    echo $hasil."<br>";
